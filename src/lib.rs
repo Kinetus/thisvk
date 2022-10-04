8288b6e8eea25fa5b4df812919254e45fd7cf0b3
Trard <trardone@gmail.com>
2022-10-04 18:20:59 +0300

    added friends get

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@ use serde::Deserialize;
 pub use vk_method::{Method, Params};
 use async_trait::async_trait;
 
+pub type UserId = u32;
+
 #[async_trait]
 pub trait API {
     type Error;
@@ -13,16 +15,20 @@ pub trait API {
     fn users_get(&self) -> UsersGetBuilder<Self> where Self: Sized {
         UsersGetBuilder::new(&self)
     }
+
+    fn friends_get(&self) -> FriendsGetBuilder<Self> where Self: Sized {
+        FriendsGetBuilder::new(&self)
+    }
 }
 
 pub struct UsersGetBuilder<'a, A: API> {
-    pub user_ids: Option<Vec<i32>>,
-    pub user_id: Option<i32>,
+    pub user_ids: Option<Vec<UserId>>,
+    pub user_id: Option<UserId>,
     api: &'a A
 }
 
 impl<'a, A: API> UsersGetBuilder<'a, A> {
-    fn new(api: &'a A) -> UsersGetBuilder<'a, A> {
+    fn new(api: &'a A) -> Self {
         UsersGetBuilder {
             user_ids: None,
             user_id: None,
@@ -30,12 +36,12 @@ impl<'a, A: API> UsersGetBuilder<'a, A> {
         }
     }
 
-    pub fn user_id(mut self, id: i32) -> UsersGetBuilder<'a, A> {
+    pub fn user_id(mut self, id: UserId) -> Self {
         self.user_id = Some(id);
         self
     }
 
-    pub fn user_ids(mut self, mut ids: Vec<i32>) -> UsersGetBuilder<'a, A> {
+    pub fn user_ids(mut self, mut ids: Vec<UserId>) -> Self {
         match self.user_ids {
             Some(ref mut users) => {
                 users.append(&mut ids)
@@ -64,10 +70,52 @@ impl<'a, A: API> UsersGetBuilder<'a, A> {
     }
 }
 
+pub struct FriendsGetBuilder<'a, A: API> {
+    pub user_id: Option<UserId>,
+    pub count: Option<u16>,
+    api: &'a A
+}
+
+impl<'a, A: API> FriendsGetBuilder<'a, A> {
+    fn new(api: &'a A) -> Self {
+        FriendsGetBuilder {
+            user_id: None,
+            count: None,
+            api
+        }
+    }
+
+    pub fn user_id(mut self, id: UserId) -> Self {
+        self.user_id = Some(id);
+        self
+    }
+
+    pub fn count(mut self, count: u16) -> Self {
+        self.count = Some(count);
+        self
+    }
+
+    pub async fn send(self) -> Result<Vec<User>, A::Error> {
+        let mut params = Params::new();
+
+        if let Some(value) = self.user_id {
+            params.insert("user_id", value);
+        }
+
+        if let Some(value) = self.count {
+            params.insert("user_id", value);
+        }
+
+        self.api.method(
+            Method::new("users.get", params)
+        ).await
+    }
+}
+
 #[allow(dead_code)]
 #[derive(Deserialize, Debug, PartialEq)]
 pub struct User {
-    id: i32,
+    id: UserId,
     first_name: String
 }
 
